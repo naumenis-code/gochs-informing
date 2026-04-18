d7097fff319f294d6c4e1579ffefd7c12b8d086e
Илья <naumenis@gmail.com>
2026-04-18 19:24:28 +0300

    Update 08-nginx.sh

--- installer/modules/08-nginx.sh
+++ installer/modules/08-nginx.sh
@@ -441,7 +441,6 @@ server {
         proxy_pass http://127.0.0.1:8000/health;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
-        access_log off;
     }
     
     location /docs {
