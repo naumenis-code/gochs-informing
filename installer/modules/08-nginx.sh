09a235e9dd89214ed91a11df0dff9a441322e6ae
Илья <naumenis@gmail.com>
2026-04-20 10:11:14 +0300

    Update 08-nginx.sh

--- installer/modules/08-nginx.sh
+++ installer/modules/08-nginx.sh
@@ -441,7 +441,6 @@ server {
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
         proxy_read_timeout 86400;
     }
     
