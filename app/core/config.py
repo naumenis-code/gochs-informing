24a6c71a968fdfffabc50da6de5ae4386d765d65
Илья <naumenis@gmail.com>
2026-04-27 13:53:33 +0300

    Delete app directory

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-# Configuration settings for the application
-
-# Load environment variables
-import os
-
-class Config:
-    DEBUG = os.environ.get('DEBUG', False)
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
